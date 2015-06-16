9ae68f8e82e2dff3885ef2f747c751d8191dea28
Iain Watt <iainjameswatt@gmail.com>
2015-06-16 10:40:09 +0100

    added require lines to application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require_tree .
+//= require angular/angular
+@import "bootstrap-sass-official/assets/stylesheets/bootstrap-sprockets";
+@import "bootstrap-sass-official/assets/stylesheets/bootstrap";
